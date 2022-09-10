84da12f261ca1ec3df5fd87341d63165c9bcdd58
HiCosta <higor_scosta@outlook.com>
2022-09-10 13:40:42 -0300

    v1.0

---
+++ stop_car_v1.0/stop_car_v1.0.py
@@ -0,0 +1,23 @@
+#cadastro de veiculos
+
+input (str("Digite a placa do veículo de acordo com o modelo(AAA1111): ").upper)
+
+
+
+
+#definindo horario
+
+hentrada = int(input("Digite a hora de entrada do veículo:  "))
+minentrada = int(input("Digite os minutos de entrada do veículo:  "))
+
+hsaida = int(input("Digite a hora de saída do veículo:  "))
+minsaida = int(input("Digite os minutos de saída do veículo:  "))
+
+hfinal = hsaida - hentrada
+if minentrada > minsaida:
+    minfinal = (minsaida + 60) - minentrada
+    hfinal = 0
+else:
+    minfinal = minsaida - minentrada
+
+print(f"O veículo ficou no estacionamento por {hfinal} horas e {minfinal} minutos.")
