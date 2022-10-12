5795ecad4ef26c61b6b5e931ecfcc93a00687b87
HiCosta <higor_scosta@outlook.com>
2022-10-12 19:30:15 -0300

    banco de dados adicionado

---
+++ stop_car_v1.0/stop_car.sql
@@ -0,0 +1,2 @@
+-- Active: 1665356528044@@127.0.0.1@3306@db_stcar
+SELECT * from cadastro;   /* mostrar itens da tabela */
--- stop_car_v1.0/stop_car_v1.0.py
+++ stop_car_v1.0/stop_car_v1.0.py
@@ -1,12 +1,20 @@
+#conexao com banco de dados
+
+import mysql.connector
+
+con = mysql.connector.connect(host = 'localhost', database = 'db_stcar', user = 'root', password = '@@Zekinha7')
+
+
 #cadastro de veiculos
 
 from multiprocessing.forkserver import read_signed
 
 
-input (str("Digite a placa do veículo de acordo com o modelo(AAA1111) ou modelo (AAA1A11): "))
-input (str("Digite o modelo do veiculo: "))
-input (str("Digite a montadora do veiculo: "))
-int (input("Digite o ano de modelo do veiculo: "))
+placa = input (str("Digite a placa do veículo de acordo com o modelo(AAA1111) ou modelo (AAA1A11): "))
+modelo = input (str("Digite o modelo do veiculo: "))
+montadora = input (str("Digite a montadora do veiculo: "))
+ano = int (input("Digite o ano de modelo do veiculo: "))
+
 
 print("-----------------------------")
 print(f"Veiculo cadastrado !")
@@ -27,6 +35,8 @@ if minentrada > minsaida:
     hfinal = 0
 else:
     minfinal = minsaida - minentrada
+    
+
 
 print("---------------------------------------------------------------------------")
 print(f"O veículo ficou no estacionamento por {hfinal} horas e {minfinal} minutos.")
@@ -65,4 +75,22 @@ elif hfinal > 0:
 print("---------------------------------------------")
 print(f"O valor total do servico e de: R${valor}")
 print("---------------------------------------------")
+  
+    
+#crud SQL
+cursor = con.cursor()
+
+comando = f"""INSERT INTO cadastro(placa, modelo, montadora, ano, hentrada, minentrada, hsaida, minsaida) 
+VALUES ('{placa}', '{modelo}', '{montadora}', {ano}, {hentrada}, {minentrada}, {hsaida}, {minsaida})"""
+
+cursor.execute(comando) 
+con.commit()    #edita o banco de dados 
+
+
+#fechando conexao com banco de dados
+
+if con.is_connected():
+    con.close()
+    cursor.close()
+    print("Conexao com banco de dados encerrada")
 
